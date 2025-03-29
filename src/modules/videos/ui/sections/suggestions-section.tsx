20e313d8ebaa104500ca7e2dcff4da35aa640f63
Daniil Durnev <daniil3221337@gmail.com>
2025-03-29 16:10:51 +0200

    video window

--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -1,3 +1,35 @@
-export const SuggestionsSection = () => {
-  return <div>Suggestions</div>;
+"use client";
+
+import { DEFAULT_LIMIT } from "@/constants";
+import { VideoRowCard } from "@/modules/videos/ui/components/video-row-card";
+import { trpc } from "@/trpc/client";
+
+interface SuggestionsSectionProps {
+  videoId: string;
+}
+
+export const SuggestionsSection = ({ videoId }: SuggestionsSectionProps) => {
+  const [suggestions] = trpc.suggestions.getMany.useSuspenseInfiniteQuery(
+    {
+      videoId,
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return (
+    <div>
+      {suggestions.pages.flatMap((page) =>
+        page.items.map((video) => (
+          <VideoRowCard
+            key={video.id}
+            data={video}
+            size={"compact"}
+          ></VideoRowCard>
+        ))
+      )}
+    </div>
+  );
 };
