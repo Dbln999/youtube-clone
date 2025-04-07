a2a869a0088b25172bb1673b5a4dc1cec0859a97
Daniil Durnev <daniil3221337@gmail.com>
2025-04-07 20:10:26 +0300

    search page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,13 +1,14 @@
 import { categoriesRouter } from "@/modules/categories/server/procedure";
 import { commentReactionsRouter } from "@/modules/comment-reactions/server/procedures";
 import { commentsRouter } from "@/modules/comments/server/procedures";
-import { createTRPCRouter } from "../init";
+import { searchRouter } from "@/modules/search/server/procedures";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
 import { suggestionsRouter } from "@/modules/suggestions/server/procedures";
 import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
+import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
@@ -19,6 +20,7 @@ export const appRouter = createTRPCRouter({
   comments: commentsRouter,
   commentReactions: commentReactionsRouter,
   suggestions: suggestionsRouter,
+  search: searchRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
