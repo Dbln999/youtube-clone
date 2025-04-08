a9e8cc25b12df62ed5adcc38d9a4dfc3ff89f422
Daniil Durnev <daniil3221337@gmail.com>
2025-04-08 15:43:19 +0300

    history

---
+++ src/modules/playlists/ui/sections/history-videos-section.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from "@/modules/videos/ui/components/video-grid-card";
+import {
+  VideoRowCard,
+  VideoRowCardSkeleton,
+} from "@/modules/videos/ui/components/video-row-card";
+
+import { DEFAULT_LIMIT } from "@/constants";
+import { ErrorBoundary } from "react-error-boundary";
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import { Suspense } from "react";
+import { trpc } from "@/trpc/client";
+
+const HistoryVideosSectionsSkeleton = () => {
+  return (
+    <div>
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {Array.from({ length: 18 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {Array.from({ length: 18 }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} size={"compact"} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const HistoryVideosSections = () => {
+  return (
+    <Suspense fallback={<HistoryVideosSectionsSkeleton />}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <HistoryVideosSectionsSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+const HistoryVideosSectionsSuspense = () => {
+  const [videos, query] = trpc.playlists.getHistory.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+  return (
+    <div>
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {videos.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+          ))}
+      </div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {videos.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoRowCard key={video.id} data={video} size={"compact"} />
+          ))}
+      </div>
+      <InfiniteScroll
+        fetchNextPage={query.fetchNextPage}
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+      />
+    </div>
+  );
+};
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,8 @@
 import { categoriesRouter } from "@/modules/categories/server/procedure";
 import { commentReactionsRouter } from "@/modules/comment-reactions/server/procedures";
 import { commentsRouter } from "@/modules/comments/server/procedures";
+import { createTRPCRouter } from "../init";
+import { playlistsRouter } from "@/modules/playlists/server/procedures";
 import { searchRouter } from "@/modules/search/server/procedures";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
@@ -8,7 +10,6 @@ import { suggestionsRouter } from "@/modules/suggestions/server/procedures";
 import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
-import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
@@ -21,6 +22,7 @@ export const appRouter = createTRPCRouter({
   commentReactions: commentReactionsRouter,
   suggestions: suggestionsRouter,
   search: searchRouter,
+  playlists: playlistsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
